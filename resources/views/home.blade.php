3481249d5dccf63c685814b2a841e1d328946278
Jesse D <jesse.davidson@umconnect.umt.edu>
2019-04-09 12:39:58 -0600

    added color map data and files, route for home.html

---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8">
+<head>
+	<title>Your Title Here</title>
+	
+	<!--Libraries------>
+	<script src="https://d3js.org/d3.v4.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<!----------------->
+	
+	<!--Linking files-->
+	<link rel="stylesheet" href="cssStyle.css">
+	<script type="text/javascript" src="{{ URL::asset('js/firstD3Script.js') }}"></script>
+	<!----------------->
+	)
+	
+	
+</head>
+<body>
+
+	<div id="container">
+		<center>
+		<button type="button" class="dataButtons">bed</button>
+		<button type="button" class="dataButtons">smb</button>
+		<button type="button" class="dataButtons">surface</button>
+		<button type="button" class="dataButtons">t2m</button>
+		<button type="button" class="dataButtons">thickness</button>
+		<button type="button" class="dataButtons">velocity</button>
+		</center>
+	</div>
+
+</body>
+</html>
